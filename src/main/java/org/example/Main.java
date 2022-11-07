782180aa50d66f1b716c38e1a75ceef63e5ce1f1
daya <dayajuangmahaputra@gmail.com>
2022-11-07 18:56:12 +0900

    Initial Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,27 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] testInput = {2500, -1024, 5000};
+
+        for(int v:testInput)
+        {
+            printMegaBytesAndKiloBytes(v);
+        }
+    }
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+
+        if(kiloBytes < 0){
+            System.out.println("Invalid Value");
+            return;
+        }
+
+        var MEGA_TO_KILO = 1024;
+        int result = kiloBytes / MEGA_TO_KILO;
+        int remainder = kiloBytes % MEGA_TO_KILO;
+
+        System.out.println(kiloBytes + " KB = " + result + " MB and " + remainder + " KB");
+
     }
 }
