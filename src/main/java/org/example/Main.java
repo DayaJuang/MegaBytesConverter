bf58e79f5c50becde5d7be9023011d8cf9ade2af
daya <dayajuangmahaputra@gmail.com>
2022-11-07 18:59:40 +0900

    Initial Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 public class Main {
+    static final int MEGA_TO_KILO = 1024;
+
     public static void main(String[] args) {
         int[] testInput = {2500, -1024, 5000};
 
@@ -11,17 +13,16 @@ public class Main {
     }
 
     public static void printMegaBytesAndKiloBytes(int kiloBytes){
-
+        // Check if the value is valid
         if(kiloBytes < 0){
             System.out.println("Invalid Value");
             return;
         }
 
-        var MEGA_TO_KILO = 1024;
-        int result = kiloBytes / MEGA_TO_KILO;
-        int remainder = kiloBytes % MEGA_TO_KILO;
+        int result = kiloBytes / MEGA_TO_KILO;      // Convert KB to MB
+        int remainder = kiloBytes % MEGA_TO_KILO;   // Check if there is a remainder
 
+        // Print the result
         System.out.println(kiloBytes + " KB = " + result + " MB and " + remainder + " KB");
-
     }
 }
